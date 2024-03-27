d8ca4d03be390d00ae71082ad6b08ce1325defae
radmanplays <95340057+radmanplays@users.noreply.github.com>
2024-03-27 12:40:16 +0330

    Update Xray.js

--- CommunityMods/Xray.js
+++ CommunityMods/Xray.js
@@ -1,5 +1,3 @@
-
-
 //Coalest xray mod to ever exist!
 
 //IIFE. I like scoped variables.
@@ -16,7 +14,7 @@
           }
         }
     })
-    var targets = ["diamond_block","diamond_ore","gold_block","gold_ore","iron_block","iron_ore","coal_block","coal_ore"]; //The target blocks
+    var targets = ["diamond_block","diamond_ore","gold_block","gold_ore","iron_block","iron_ore","coal_block","coal_ore","emerald_ore","emerald_block","redstone_ore","redstone_block","lapis_ore","lapis_block","chest","furnace","lit_furnace","ender_chest"]; //The target blocks
     var allblocks = Object.keys(ModAPI.blocks); //List of all block IDsw
     function update() {
       ModAPI.displayToChat({msg: "xray Enabled!"})
