c569fb4acebe50e2e71793e767948f227077c510
SpeedSlicer <143555953+SpeedSlicer@users.noreply.github.com>
2024-09-03 23:06:39 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000;
+const port = 443;
 
 app.use(express.static(path.join(__dirname, 'public')));
 
